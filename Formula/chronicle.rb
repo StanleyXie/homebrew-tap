65e2fb3de7ee3a91a130595ae1ba36b11da76b91
Stanley <stanley.xie@outlook.com>
2026-01-06 15:09:48 +0100

    chore: update formula to v0.2.2

--- Formula/chronicle.rb
+++ Formula/chronicle.rb
@@ -1,11 +1,11 @@
 class Chronicle < Formula
   desc "AI conversation history extraction and observability tool"
   homepage "https://github.com/StanleyXie/chronicle"
-  version "0.2.1"
+  version "0.2.2"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/StanleyXie/chronicle/releases/download/v0.2.1/chronicle-darwin-arm64.tar.gz"
-    sha256 "faa2235dff9cb841ef022466bb0f41987461c2f380b0196b8a88b3a170f69cab"
+    url "https://github.com/StanleyXie/chronicle/releases/download/v0.2.2/chronicle-darwin-arm64.tar.gz"
+    sha256 "b3c301c0bcc0d9d76359d19b8af80d2d0e6914211620fa1ea103a8f3be54207e"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/StanleyXie/chronicle/releases/download/v#{version}/chronicle-darwin-amd64.tar.gz"
   else

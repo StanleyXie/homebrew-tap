256143b097afd4de2589f536c6f8b163100d283a
StanleyXie <stanley.xie@outlook.com>
2026-01-06 14:40:19 +0100

    chore: update formula to support binary installation

--- Formula/chronicle.rb
+++ Formula/chronicle.rb
@@ -1,17 +1,24 @@
 class Chronicle < Formula
   desc "AI conversation history extraction and observability tool"
   homepage "https://github.com/StanleyXie/chronicle"
-  url "https://github.com/StanleyXie/chronicle/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "3d88ed8581a634b356f52bb7440ac9c66545edde2fd4c7f3c7e2e1bf6fb71802"
+  version "0.2.0"
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/StanleyXie/chronicle/releases/download/v#{version}/chronicle-darwin-arm64.tar.gz"
+    # sha256 will be updated by GitHub Action
+  elsif OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/StanleyXie/chronicle/releases/download/v#{version}/chronicle-darwin-amd64.tar.gz"
+  else
+    url "https://github.com/StanleyXie/chronicle/releases/download/v#{version}/chronicle-linux-amd64.tar.gz"
+  end
+
   license "MIT"
 
-  depends_on "rust" => :build
-
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "chronicle"
   end
 
   test do
-    assert_match "chronicle #{version}", shell_output("#{bin}/chronicle --version")
+    assert_match "chronicle", shell_output("#{bin}/chronicle --help")
   end
 end
